949133a17e8323991468a7e8bda1bffad70a2095
Tryhard <daongoctruong10042000@gmail.com>
2024-01-05 16:07:32 +0700

    bai_tap_1

---
+++ lib.h
@@ -0,0 +1,3 @@
+int sum(int a, int b){
+    printf("tong %d",a + b);   
+}
---
+++ tesst1111.c
@@ -0,0 +1,12 @@
+#include "lib.h"
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+    sum(1,1);
+    int a;
+    scanf("%d",&a);
+    printf("ABC\n");
+    return 0;
+}
