30d464a351928110428c17b1b89e1771ce1fdfa4
Tryhard <daongoctruong10042000@gmail.com>
2024-01-12 06:54:12 +0700

    bt4

--- tesst1111.c
+++ tesst1111.c
@@ -1,45 +1,23 @@
 // #include "lib.h"
 
 #include <stdio.h>
-struct Student{
-    char name[100];
-    int toan;
-    int van;
-    int anh;
-    int ly;
-    int hoa;
-};
-void input(struct Student a[], int n){
-    for(int i = 0; i < n; i++){
-        printf("Ten hoc sinh: \n");
-        scanf("%s",&a[i].name);
-        printf("Diem Toan: \n");
-        scanf("%d",&a[i].toan);
-        printf("Diem van: \n");
-        scanf("%d",&a[i].van);
-        printf("Diem anh: \n");
-        scanf("%d",&a[i].anh);
-        printf("Diem ly: \n");
-        scanf("%d",&a[i].ly);
-        printf("Diem hoa: \n");
-        scanf("%d",&a[i].hoa);
-    }
-}
-void print(struct Student a[],int n){
-    for(int i = 0; i < n ; i++){
-        printf("Ten hoc sinh: %s \n",a[i].name);
-        printf("Diem Toan: %d\n",a[i].toan);
-        printf("Diem van: %d \n",a[i].van);
-        printf("Diem anh: %d\n",a[i].anh);
-        printf("Diem ly: %d\n",a[i].ly);
-        printf("Diem hoa: %d\n",a[i].hoa);
-    }    
+#include <string.h>
 
-}
 int main(){
-    int n;
-    struct Student student1[n];
-    input(student1,2);
-    print(student1,2);
+    char str[] = "             hello world toi ten la ngoc truong ";
+    int a = strlen(str);
+    int word = 0;
+    for(int i = 0; i < a; i++){
+        if(str[i] == ' ' && str[i+1] == ' '){
+            continue;
+        }
+        else if(str[i] == ' ' && str[i+1] != ' ' && str[i+1] != '\0'){
+            word++;
+        }
+        else{
+            continue;
+        }
+    }
+    printf("so tu trong chuoi:  %d",word);
     return 0;
 }
