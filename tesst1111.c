4ddbd470420306d342108887e123ee8302575c69
Tryhard <daongoctruong10042000@gmail.com>
2024-03-03 22:21:18 +0700

    MOCK_C_FPT

--- tesst1111.c
+++ tesst1111.c
@@ -1,100 +1,185 @@
-/*Pointer advance assignment
-
-Cho một mảng gồm 20 bytes “uint8_t assignment4[20] = {0xFF, 0xFF,…,0xFF}” dùng để lưu trữ các giá trị từ 0 đến 100.
-
-Dùng danh sách liên kết đơn để quản lý các phần tử trong mảng theo giá trị từ thấp đến cao và viết “menu” chương trình cho phép người dùng:
-
-+ Nhập 1 giá trị vào 1 phần tử chỉ định của mảng trên (Dùng phím 1 để chọn option này)
-    Vị trí từ 0 đến 19. Báo lỗi nếu:
-        Vị trí người dùng chọn ngoài khoảng 0 đến 19
-        Vi trí đã được nhập rồi.
-    Giá trị từ 0 đến 100. Báo lỗi nếu:
-        Giá trị người dùng chọn ngoài khoảng 0 đến 100
-        Giá trị người dùng chọn đã tồn tại ở trong mảng.
-+ Xóa 1 giá trị nào đó trong mảng trên (Dùng phím 2 để chọn option này)
-    Sau khi chọn option này, người dùng sẽ chọn 1 giá trị từ 0 đến 100 để xóa, ví dụ người dùng chọn 50:
-        Nếu giá trị này nằm trong mảng trên thì xóa phần tử này khỏi mảng.
-        Nếu giá trị này không nằm trong mảng, báo lỗi không tồn tại.
-    Nếu giá trị người dùng chọn ngoài khoảng từ 0 đến 100, báo lỗi giá trị chọn không đúng.
-+ In giá trị các phần tử được nhập vào mảng (Dùng phím 3 để chọn option này)
-    Sau khi chọn option này, người dùng chọn
-        1: In các giá trị đã được nhập của mảng khi chưa sắp xếp từ thấp đến cao.
-        2: In các giá trị đã được nhập của mảng sau khi đã sắp xếp (dùng danh sách liên kết đơn theo yêu cầu ban đầu).
-Kết thúc chương trình (Dùng phím 4 để chọn option này).
-
-Chú ý: Không được dùng các hàm cấp phát bộ nhớ động.*/
-
-
-
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdint.h>
+#include <time.h>
 
-struct Array{
-    int data;
-    struct Array* next;
+#define MAX_LENGTH 25
+
+
+// create struct to save player and player's score
+struct Node{
+    char Player[MAX_LENGTH];
+    int one_c = 0;
+    int two_c = 0;
+    int three_c = 0;
+    int four_c = 0;
+    int score = 0;
+    int number_guests;
 };
 
-// insert_value
-void insert_value(uint8_t a[],int position,int value){
-    //check range
-    if(position > 19 && position < 0){
-        printf("Position is out of range");
+void save_info_player(struct Node Table[],int &times){
+    FILE *file = fopen("Player.txt", "a");
+    if (file == NULL){
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
     }
-    // check available position
-    if(value != 0xFF){
-        printf("This location has a value entered");
+    for (int i = 0; i < times; i++) {
+        fprintf(file, "%s %d %d %d %d %d %d\n", Table[i].Player, Table[i].one_c, Table[i].two_c,Table[i].three_c, Table[i].four_c, Table[i].score, Table[i].number_guests);
     }
-    // check value
-    if( value < 0  && value > 100)
-        printf("Value is out of range");
-    }
-    // check  value exists or not
-    struct Array* head = &a[0];
-    head->data = a[0];
-    head->next = NULL;
 
-    struct Array* root = head;
-    while (current != NULL) {
-        if (current->data == value) {
-            printf("Lỗi: Giá trị đã tồn tại trong mảng.\n");
-        }
-        current = current->next;
+    fclose(file);
+    
+}
+void printFromFile(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return;
     }
-    assignment4[position] = value;
+
+    struct Node PL;
+    while (fscanf(file, "%s %d %d %d %d %d %d\n", PL.Player, &PL.one_c, &PL.two_c,&PL.three_c, &PL.four_c, &PL.score, &PL.number_guests) == 7) {
+        printf("Name: %s, Score: %d, number_guests: %d\n", PL.Player, PL.score, PL.number_guests);
+    }
+	if (feof(file)) {
+        printf("End of file reached.\n");
+    } else if (ferror(file)) {
+        perror("Error reading from file");
+    }
+    fclose(file);
 }
 
+// create the function random number generator
+int random(int min, int max){
+    return min + rand() % (max + 1 - min);
+} 
 
+// function compare magic numbers and number of players
+void compare_number1(int &magic_number, struct Node Table[], int &dem,const int a[]){
+    
+    for(int i = 0; i < 4; i++){
+        if(Table[dem].one_c == a[i]){
+            printf("%d", i);
+            // printf("table: %d\n",Table[dem].one_c );
+            // printf("a[%d]: %d\n",i,a[i] );
+            Table[dem].score += 1;
+        }
+		else{
+            printf("-");
+        }
+    }
+    printf("\n");
+    
+}
+// function compare magic numbers and number of players
+void compare_number2(int &magic_number, struct Node Table[], int &dem,const int a[]){
+    
+    for(int i = 0; i < 4; i++){
+        if(Table[dem].two_c == a[i]){
+            printf("%d", i);
+            // printf("table: %d\n",Table[dem].two_c );
+            // printf("a[%d]: %d\n",i,a[i] );
+            Table[dem].score += 1;
+        }
+		else{
+            printf("-");
+        }
+    }
+    printf("\n");
+    
+}
+// function compare magic numbers and number of players
+void compare_number3(int &magic_number, struct Node Table[], int &dem,const int a[]){
+    
+    for(int i = 0; i < 4; i++){
+        if(Table[dem].three_c == a[i]){
+            printf("%d", i);
+            // printf("table: %d\n",Table[dem].three_c );
+            // printf("a[%d]: %d\n",i,a[i] );
+            Table[dem].score += 1;
+        }
+		else{
+            printf("-");
+        }
+    }
+    printf("\n");
+    
+}
+// function compare magic numbers and number of players
+void compare_number4(int &magic_number, struct Node Table[], int &dem,const int a[]){
+    
+    for(int i = 0; i < 4; i++){
+        if(Table[dem].four_c == a[i]){
+            printf("%d", i);
+            // printf("table: %d\n",Table[dem].four_c );
+            // printf("a[%d]: %d\n",i,a[i] );
+            Table[dem].score += 1;
+        }
+		else{
+            printf("-");
+        }
+    }
+    printf("\n");
+    
+}
 // MAIN Function
 int main(){
-    uint8_t assignment4[20] = = {0xFF, 0xFF,0xFF, 0xFF,0xFF, 0xFF,0xFF, 0xFF,0xFF, 0xFF,0xFF, 0xFF,0xFF, 0xFF,0xFF, 0xFF,0xFF, 0xFF,0xFF, 0xFFF}; // array have 20 element
-    uint8_t value;
-    uint8_t position;
-    int choice;
-    printf("\nMenu:\n");
-    printf("1. Insert value\n");
-    printf("2. Delete value\n");
-    printf("3. Print array\n");
-    printf("4. Exit\n");
-    printf("Select an option: ");
-    scanf("%d",&choice);
-    switch (choice){
-        case 1 :
-            printf("Enter position: ");
-            scanf("%d", &position);
-            printf("Enter value (0 to 100): ");
-            scanf("%d", &value);
-            insert_value(position,value);
-            break;
-        case 2 :
-            break;
-        case 3 :
-            break;
-        case 4 :
-            break;
-        default:
-            break
-    }
+    // create a array struct to save player and player's score
+    struct Node Table[100];
+
+    // print game's name
+    printf("------------Number Guesting Game------------------\n");
+    
+    // create the number player must guest
+    // number of enter
+    int dem = 0;
+    int times = 1;
+    char choose;
+    do{
+        // print request to player enter their player's name
+
+        // create the number player must guest
+	    srand((int) time(0));
+	    int magic_number = random (0,9999);
+	
+	    int ngan = magic_number / 1000;
+	    int tram = (magic_number / 100) - ngan*10;
+	    int chuc = (magic_number / 10) - ngan*100 - tram*10;
+	    int donvi = magic_number - ngan*1000 - tram*100 -  chuc*10;
+	
+	    int a[4] = {ngan, tram, chuc, donvi};
+		Table[dem].number_guests = magic_number;
+        printf("magic number %d\n",magic_number);
+        printf("Enter your player: " );
+        scanf("%s",&Table[dem].Player);
+
+        printf("Enter number: ");
+        scanf(" %d",&Table[dem].one_c);
+        compare_number1(magic_number,Table,dem,a);
+
+        printf("Enter number: ");
+        scanf(" %d",&Table[dem].two_c);
+        compare_number2(magic_number,Table,dem,a);
+
+        printf("Enter number: ");
+        scanf(" %d",&Table[dem].three_c);
+        compare_number3(magic_number,Table,dem,a);
+
+        printf("Enter number: ");
+        scanf(" %d",&Table[dem].four_c);
+        compare_number4(magic_number,Table,dem,a);
+        dem++;
+        if(dem == 5){
+            dem = 0;
+            times++;
+        }
+        save_info_player(Table,times);
+        printf("Do you want to continue? [y/n]?");
+        scanf(" %s",&choose);
+    }while(choose == 'Y' || choose == 'y');
+    char conti;
+    printf("Do you want to print history? [y/n]?");
+    scanf("%s",&conti);
+    printFromFile("Player.txt");
     return 0;
 }
